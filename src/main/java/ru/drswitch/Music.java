5c66ef1939f74c7d9ab153ec44001b850142b418
DrSwitch <switchyndra@gmail.com>
2019-03-21 20:53:23 +0800

    first commit

---
+++ src/main/java/ru/drswitch/Music.java
@@ -0,0 +1,5 @@
+package ru.drswitch;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/ru/drswitch/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.drswitch;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cris Marry";
+    }
+}
